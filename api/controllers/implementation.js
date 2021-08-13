bd8c82e5ae57b7e5414ca7ef63182e7c127e8ecc
Nirmal Kumar H <nirmal@appveen.com>
2021-08-14 02:01:21 +0530

    Update implementation.js

--- api/controllers/implementation.js
+++ api/controllers/implementation.js
@@ -13,33 +13,39 @@ const list = (_req, _res) => {
     let page = _req['query']['page'] ? parseInt(_req['query']['page']) : 1; // page of the result to be returned
     let count = _req['query']['count'] ? parseInt(_req['query']['count']) : '';  // number of result to be returned. If not specified, it will return all records
 
-    let aggregateQuery = [
-        {
-            $project: {
-                _id: 0,
-                key: 1,
-                createdAt: 1,
-                totalCount: { $sum: '$counts' }
+    return Promise.resolve()
+        .then(() => {
+            if (isNaN(new Date(ip.startDate).valueOf()) ||
+                isNaN(new Date(ip.endDate).valueOf())) {
+                throw 'Please pass valid date';
             }
-        },
-        {
-            $match: {
-                totalCount: { $gte: ip.minCount, $lte: ip.maxCount },
-                createdAt: { $gte: new Date(ip.startDate), $lte: new Date(ip.endDate) }
-            }
-        },
-        ...(
-            (count)
-                ? [
-                    { $skip: (page - 1) * count },
-                    { $limit: count }
-                ]
-                : []
-        )
-    ]
-
-    return mongoose.model('records').aggregate(aggregateQuery)
-        .option({ maxTimeMS: mongoQueryTimeOut, allowDiskUse: true })
+            let aggregateQuery = [
+                {
+                    $project: {
+                        _id: 0,
+                        key: 1,
+                        createdAt: 1,
+                        totalCount: { $sum: '$counts' }
+                    }
+                },
+                {
+                    $match: {
+                        totalCount: { $gte: ip.minCount, $lte: ip.maxCount },
+                        createdAt: { $gte: new Date(ip.startDate), $lte: new Date(ip.endDate) }
+                    }
+                },
+                ...(
+                    (count)
+                        ? [
+                            { $skip: (page - 1) * count },
+                            { $limit: count }
+                        ]
+                        : []
+                )
+            ];
+            return mongoose.model('records').aggregate(aggregateQuery)
+                .option({ maxTimeMS: mongoQueryTimeOut, allowDiskUse: true });
+        })
         .then(doc => {
             _output['records'] = doc;
             return _res.status(200).json(_output);
